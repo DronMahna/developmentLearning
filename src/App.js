96a22817dd13e2db505f94685ad35a2a8fbc4818
DronMahna <dronmahna@gmail.com>
2021-01-28 21:01:13 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -6,17 +6,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        
+        <div
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Learning Development test 2
+        </div>
       </header>
     </div>
   );
